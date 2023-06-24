9db2f37c5bfe34d13ef5126168e07f15166cbbec
Victor <vkunssec@gmail.com>
2023-06-23 23:33:37 -0300

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+# oh-my-posh init pwsh | Invoke-Expression
+
+
+Import-Module oh-my-posh
+Import-Module posh-git
+
+
+# modulo de icones
+Import-Module -Name Terminal-Icons
+
+# customizacao oh my posh
+oh-my-posh init pwsh --config 'C:\Users\victo\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
+
+
+
+# limpar terminal
+Clear-Host
