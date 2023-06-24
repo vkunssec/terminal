0dadb30d5dbeef5f1be69f996545199958ab868d
Victor <vkunssec@gmail.com>
2023-06-24 02:00:02 -0300

    feat: personalizacao

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,8 @@ Import-Module posh-git
 Import-Module -Name Terminal-Icons
 
 # customizacao oh my posh
-oh-my-posh init pwsh --config 'C:\Users\victo\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\victo\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\victo\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1\PowerShell\custom.omp.json' | Invoke-Expression
 
 
 
